742c0ec1c4c49ebd52aabb88690e2363e27547cf
Rodolphe Deneux <raiden13008@gmail.com>
2018-01-11 12:05:29 +0100

    add pizzaPool

--- src/java/com/dmr/pizzaweb/servlet/LoginServlet.java
+++ src/java/com/dmr/pizzaweb/servlet/LoginServlet.java
@@ -19,44 +19,6 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "LoginServlet", urlPatterns = {"/Login"})
 public class LoginServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-            
-            response.setContentType("text/html;charset=UTF-8");
-            
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            User user;
-            
-        try {            
-            user = new User(username, password);
-            
-            HttpSession session = request.getSession();
-            session.setAttribute("user", user);
-            
-            // Redirection http 302
-            // response.sendRedirect("app.jsp");
-            
-            RequestDispatcher dispatcher = request.getRequestDispatcher("app.jsp");
-            dispatcher.forward(request, response);
-            
-        } catch (Exception ex) {
-            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("error.jsp");
-            dispatcher.forward(request, response);
-        }
-    }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -69,7 +31,33 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        response.setContentType("text/html;charset=UTF-8");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        HttpSession session = request.getSession();
+        RequestDispatcher dispatcher;
+            
+        try {            
+            User user = new User(username, password);
+            
+            
+            session.setAttribute("user", user);
+            
+            // Redirection http 302
+            // response.sendRedirect("app.jsp");
+            
+            dispatcher = request.getRequestDispatcher("app.jsp");
+            dispatcher.forward(request, response);
+            
+        } catch (Exception ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+            dispatcher = request.getRequestDispatcher("error.jsp");
+            dispatcher.forward(request, response);
+        }
+        
     }
 
     /**
